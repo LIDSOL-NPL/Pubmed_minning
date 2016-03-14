a7f50c5cd5677ca77195fdbd2a95edec3a522949
raulmejia <raulmejia@users.noreply.github.com>
2016-03-13 20:08:41 -0500

    Update Cosine_Similarity.py

--- Cosine_Similarity.py
+++ Cosine_Similarity.py
@@ -1,21 +1,37 @@
-#import argparse
-#from pprint import pprint
+import argparse
+from pprint import pprint
 
-#parser = argparse.ArgumentParser(description='cos classify some files')
-#parser.add_argument('--archivos', type=argparse.FileType('r'), required=False, nargs='*')
-#args = parser.parse_args()
+parser = argparse.ArgumentParser(description='cos classify some files')
+parser.add_argument('--archivos', type=argparse.FileType('r'), required=False, nargs='*')
+args = parser.parse_args()
 
-# args tiene una lista de archivos abiertos! checa:
-#pprint(args.archivos)
+  args tiene una lista de archivos abiertos! checa:
+pprint(args.archivos)
 
 # puedes hacer algo como:
 #for archivo in args.archivos:
 #    pprint(archivo.read()[0:70])
 
 
-
+import argparse
 import glob
-path="/home/neo/Documents/Trabajo/Trabajo_academico/CursoCCGBioinfo/Carlos_Natural_Lenguage_Procesing/LearningSciKit/"
+
+parser = argparse.ArgumentParser()
+parser.add_argument("path", help="Please provide the exact and complete  path t$
+args = parser.parse_args()
+
+####
+path= args
+flag=  path + "*.txt"
+print flag
+###
+
+print args
+#print args.echo
+
+#path="/home/neo/Documents/Trabajo/Trabajo_academico/CursoCCGBioinfo/Carlos_Natural_Lenguage_Procesing/LearningSciKit/"
+path= args
+
 Corpus_titles = glob.glob( path + "*.txt")
 print Corpus_titles 
 
@@ -25,5 +41,5 @@ for title in Corpus_titles:
     with open(title,'r') as myfile:
         data=myfile.read().replace('\n',' ')
         print data;
-        
-    Corpus.append(data)
+        Corpus.append(data)
+
