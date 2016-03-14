c7f4abd78dd33a37843262b55199a3d67f0b2573
Rodrigo Garcia <rgarcia@inmegen.gob.mx>
2016-03-13 22:20:57 -0600

    print titles

--- Cosine_Similarity.py
+++ Cosine_Similarity.py
@@ -10,8 +10,10 @@ parser = argparse.ArgumentParser(description='cos classify some files')
 parser.add_argument('archivos', type=argparse.FileType('r'), nargs='+')
 args = parser.parse_args()
 
+titulos=[]
 Corpus=[]
 for archivo in args.archivos:
+    titulos.append(archivo.name)
     Corpus.append(archivo.read())    
 
 
@@ -19,15 +21,15 @@ documents=Corpus
 tfidf_vectorizer = TfidfVectorizer()
 tfidf_matrix     = tfidf_vectorizer.fit_transform(documents)
 
-print tfidf_matrix.shape
-
 
 cosine_similarity(tfidf_matrix[:], tfidf_matrix)
-print cosine_similarity(tfidf_matrix[:], tfidf_matrix)
+for row in cosine_similarity(tfidf_matrix[:], tfidf_matrix):
+    pprint(row)
 
+pprint(titulos)
 
-names=[]    
-for i in range(len(Corpus_titles)):
-    names.append(Corpus_titles[i][len(path)+1:])
+# names=[]    
+# for i in range(len(Corpus_titles)):
+#     names.append(Corpus_titles[i][len(path)+1:])
 
-print names
+# print names
